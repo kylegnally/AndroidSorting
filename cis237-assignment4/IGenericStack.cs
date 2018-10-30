bb3186837c26aa737bc7d14c5cdf32b9cd4d0954
Kyle Nally <kylegnally@gmail.com>
2018-10-30 14:27:09 -0400

    Add interface and classes for queue and stack.

---
+++ cis237-assignment4/IGenericStack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237_assignment4
+{
+    interface IGenericStack
+    {
+        void Push(Droid droid);
+        void Pop();
+
+        bool isEmpty;
+        int size;
+    }
+}
