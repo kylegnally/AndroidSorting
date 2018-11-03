a66302b79971262366414f3c3dc13fe0a88aef21
Kyle Nally <kylegnally@gmail.com>
2018-11-03 18:08:14 -0400

    Change method names in MergeSort class to use proper naming conventions. Remove unneeded methods in MergeSort class.

--- cis237-assignment4/MergeSort.cs
+++ cis237-assignment4/MergeSort.cs
@@ -41,15 +41,15 @@ namespace cis237_assignment4
         public MergeSort(IDroid[] droids)
         {
             Droids = droids;
-            sort(Droids);
+            Sort(Droids);
         }
 
         // stably merge a[lo .. mid] with a[mid+1 ..hi] using aux[lo .. hi]
-        private static void merge(IDroid[] a, IDroid[] aux, int lo, int mid, int hi)
+        private static void Merge(IDroid[] a, IDroid[] aux, int lo, int mid, int hi)
         {
             // precondition: a[lo .. mid] and a[mid+1 .. hi] are sorted subarrays
-            Debug.Assert(isSorted(a, lo, mid));
-            Debug.Assert(isSorted(a, mid+1, hi));
+            Debug.Assert(IsSorted(a, lo, mid));
+            Debug.Assert(IsSorted(a, mid+1, hi));
 
             // copy to aux[]
             for (int k = lo; k <= hi; k++)
@@ -63,29 +63,29 @@ namespace cis237_assignment4
             {
                 if (i > mid) a[k] = aux[j++];
                 else if (j > hi) a[k] = aux[i++];
-                else if (less(aux[j], aux[i])) a[k] = aux[j++];
+                else if (Less(aux[j], aux[i])) a[k] = aux[j++];
                 else a[k] = aux[i++];
             }
 
             // postcondition: a[lo .. hi] is sorted
-            Debug.Assert(isSorted(a, lo, hi));
+            Debug.Assert(IsSorted(a, lo, hi));
         }
 
         // mergesort a[lo..hi] using auxiliary array aux[lo..hi]
         // this is called from inside the class
-        private static void sort(IDroid[] a, IDroid[] aux, int lo, int hi)
+        private static void Sort(IDroid[] a, IDroid[] aux, int lo, int hi)
         {
             if (hi <= lo) return; // this is the base case
                                   // it's importat that the midpoint is done this way
             int mid = lo + (hi - lo) / 2;
             // left half
-            sort(a, aux, lo, mid);
+            Sort(a, aux, lo, mid);
 
             // right half
-            sort(a, aux, mid + 1, hi);
+            Sort(a, aux, mid + 1, hi);
 
             // merge them
-            merge(a, aux, lo, mid, hi);
+            Merge(a, aux, lo, mid, hi);
         }
 
         /**
@@ -95,11 +95,11 @@ namespace cis237_assignment4
         // this is called from the outside
         // the methods have different signatures so the recursive call will be made
         // to the above method
-        public static void sort(IDroid[] a)
+        public static void Sort(IDroid[] a)
         {
             IDroid[] aux = new IDroid[a.Length];
-            sort(a, aux, 0, a.Length- 1);
-            Debug.Assert(isSorted(a));
+            Sort(a, aux, 0, a.Length- 1);
+            Debug.Assert(IsSorted(a));
         }
 
 
@@ -108,7 +108,7 @@ namespace cis237_assignment4
          ***************************************************************************/
 
         // is v < w ?
-        private static bool less(IDroid v, IDroid w)
+        private static bool Less(IDroid v, IDroid w)
         {
             if (v == null || w == null) return false;
             return v.CompareTo(w) < 0;
@@ -117,51 +117,16 @@ namespace cis237_assignment4
         /***************************************************************************
          *  Check if array is sorted - useful for debugging.
          ***************************************************************************/
-        private static bool isSorted(IDroid[] a)
+        private static bool IsSorted(IDroid[] a)
         {
-            return isSorted(a, 0, a.Length - 1);
+            return IsSorted(a, 0, a.Length - 1);
         }
 
-        private static bool isSorted(IDroid[] a, int lo, int hi)
+        private static bool IsSorted(IDroid[] a, int lo, int hi)
         {
             for (int i = lo + 1; i <= hi; i++)
-                if (less(a[i], a[i - 1])) return false;
+                if (Less(a[i], a[i - 1])) return false;
             return true;
-        }
-
-
-        /***************************************************************************
-         *  Index mergesort.
-         ***************************************************************************/
-        // stably merge a[lo .. mid] with a[mid+1 .. hi] using aux[lo .. hi]
-        private static void merge(IDroid[] a, int[] index, int[] aux, int lo, int mid, int hi)
-        {
-
-            // copy to aux[]
-            for (int k = lo; k <= hi; k++)
-            {
-                aux[k] = index[k];
-            }
-
-            // merge back to a[]
-            int i = lo, j = mid + 1;
-            for (int k = lo; k <= hi; k++)
-            {
-                if (i > mid) index[k] = aux[j++];
-                else if (j > hi) index[k] = aux[i++];
-                else if (less(a[aux[j]], a[aux[i]])) index[k] = aux[j++];
-                else index[k] = aux[i++];
-            }
-        }
-
-        // mergesort a[lo..hi] using auxiliary array aux[lo..hi]
-        private static void sort(IDroid[] a, int[] index, int[] aux, int lo, int hi)
-        {
-            if (hi <= lo) return;
-            int mid = lo + (hi - lo) / 2;
-            sort(a, index, aux, lo, mid);
-            sort(a, index, aux, mid + 1, hi);
-            merge(a, index, aux, lo, mid, hi);
         }
     }
 }
